f0d35d529b2c003e4ef604610a4de7b852129e24
Harryrana98 <ranaharendra77@gmail.com>
2024-09-27 18:06:42 +0530

    new

---
+++ Javascript ClassRoom/index.js
@@ -0,0 +1,209 @@
+// document.write("welcome to basepur")
+// console.log("welcome to javascript")
+// alert("welcome to js")
+
+
+// var a=90
+
+
+// console.log(a)
+
+// alert(a)
+
+// document.write(a)
+
+
+// var mynameislakhan="100000"
+
+// console.log(mynameislakhan)
+// alert(mynameislakhan)
+// document.write(mynameislakhan)
+
+
+// var x=5 
+// var y=6
+// var z=x+y
+// console.log(z)
+// alert(z)
+// document.write(z)
+
+
+
+// var = redeclare yes - reassign yes- global scope
+
+// var myname="harry"
+
+// console.log(myname)
+
+// myname="harendra"
+// console.log(myname)
+
+// console.log(x)
+
+
+// var x=100
+
+// {
+
+//     console.log(x)
+// }
+
+// console.log(x)
+
+
+// let =  redeclare no - reassign yes - block scope
+
+// let a=20
+
+// let a=50    no redeclare
+
+// a=50   reassign
+
+// console.log(a)
+
+// console.log(x)
+// {
+
+//     console.log(x)
+
+//     let x=100
+
+//     console.log(x)
+// }
+
+
+// console.log(x)
+
+
+
+// const - no redeclare  - no  reassign  - block Scope
+
+// const x=100
+
+// x=100 error 
+
+// const x=100 error
+
+
+// console.log(x)
+
+// const x=500  
+// console.log(x)
+// console.log(x)
+
+
+
+
+// data Type
+
+// var a=100
+// Number
+// console.log(a)
+
+// string
+// var a="harry"
+
+// let numberConversion = Number(a)
+// console.log(typeof(numberConversion))
+
+// String
+// var a="100"
+// let numberConversion = Number(a)
+// console.log(typeof(numberConversion))
+
+// console.log(typeof(a))
+
+// Boolean
+// var a=false
+
+// let valueInboolean =Number(a)
+// console.log(typeof(valueInboolean))
+// console.log(valueInboolean)
+
+// var a="rahul";
+
+//         console.log(typeof(a));
+
+        // var data=true;
+        // console.log(typeof(data));
+        // console.log(data);
+
+        // var a;
+        // // undefine
+        // console.log(typeof(a));
+
+
+        // object null
+        // var a=null;
+        // console.log(typeof(a));
+
+
+
+        // Object  array
+        // var a=[20,30,40,50];
+        // console.log(typeof(a));
+
+
+        //   Object
+        // var a={
+        //     myname:"aman",
+        //     lname:"jain"
+        // };
+
+        // console.log(typeof(a));
+
+        // var a="aman"+ ['&nbsp' ,'&nbsp'] +" jain";
+
+        // console.log(a);
+        // document.write(a)
+
+        // var a="hello how are you";
+        // var uname=" Roy";
+
+        // console.log(a+uname)
+
+
+        // var a=90+10+"lalit";
+
+        // console.log(a);
+
+        // var a=90+"lalit"+50+50;
+
+        // console.log(a);
+
+        // var a=50+50-20+"ajeet"+50+80;
+
+        // console.log(a);
+
+
+        // var a="50"+50;
+
+        // console.log(a)
+
+        // var a="ram"*"jain";
+  
+        // not a number[NaN]
+        // console.log(a);
+
+
+        // var a=2*(50+50)-100/100*1;
+
+        // console.log(a);
+
+
+
+        // const id = Symbol('123')
+        // const anotherId = Symbol('123')
+
+        // console.log(id===anotherId);
+        
+
+
+        // const bigNumber = 563723882720823628362868262646726383n
+
+          
+
+
+
+
+
---
+++ Javascript ClassRoom/operator.js
@@ -0,0 +1,192 @@
+
+// addition
+
+// let x=5+10
+// y=7
+
+// console.log(x)
+
+
+// Area of circle formula pie r sqaure
+
+// let pie=3.14
+// r=10
+
+// console.log(pie*r*r)
+
+
+// perameter of circle 2 pie r
+
+// let pie=3.14
+// r=10
+// console.log(2*pie*r)
+
+
+// Area of rectangle l*b
+// let l=10
+// b=10
+// console.log(l*b)
+
+
+// Perimeter of rectangle 2(l+b)
+// let l=10
+// b=5
+// console.log(2*(l+b))
+
+
+// Area of square side*side
+// let side=10
+// console.log(side*side)
+
+
+
+// assignment
+
+// var a=10
+// a+=10
+// console.log(a)
+
+// var a=10
+// a*=5
+// console.log(a) 
+
+
+
+// increment or decrement 
+//   var a=1
+
+//   console.log(++a)   
+//   console.log(a++)   
+//   console.log(--a)  
+//   console.log(a--)  
+//   console.log(a)   
+//   console.log(++a) 
+//   console.log(++a) 
+//   console.log(a++) 
+//   console.log(a++) 
+//   console.log(a--) 
+//   console.log(a--) 
+
+// var a=50
+    // console.log(a);
+
+
+    // var a=50;
+    // a+=90;
+    
+    // // (a=a+90)
+
+    // console.log(a)
+
+
+    // var num=5;
+    // num/10;  
+
+    // console.log(num);
+
+    // increment/decrement
+
+    // pre increment   (++a)
+    // post increment  (a++)
+
+    // pre decrement (--a)
+    // post decrement(a--)
+
+    // var a=10;
+    // ++a;   11
+    // a++;  12
+    // a--  
+    // a--
+    // a--
+    // --a
+    // console.log(a)
+
+    // var a=1;
+
+    // console.log(++a);    
+    // console.log(a++)       
+    // console.log(a)    
+    // console.log(++a)
+    // console.log(a);
+    // console.log(a++)      
+    // console.log(a++) 
+    // console.log(a)
+    // console.log(--a)   
+    // console.log(a--)  
+    // console.log(a--)  
+    // console.log(a) 
+
+    // comparision operator
+
+    // (==,===,><,>=<=)
+
+    // var a=50;
+    // b=50;
+
+    // console.log(a<=b);
+
+
+    // var a=20;
+    // b=20;
+
+    // console.log(a=b)
+
+    // ==  value compare
+    // === value and data type compare
+    // var a="ram";
+    // var b=ram;
+
+
+    // console.log(a==b);
+
+
+    // var a=50;
+    // b='50';
+
+    // console.log(a===b);
+
+
+//     var a="50";
+//    var b=50;
+
+//     console.log(a==b)
+
+
+// if and else
+
+
+// var x=30
+// y=20
+
+
+// if user age 18 se jyda h to output aayega " person is elligible for voting"
+
+
+// var age=16
+
+// if(age>=18){
+//     console.log("person is elligible for voting")
+// }
+
+// else{
+//     console.log("person is not elligible for voting")
+// }
+
+
+
+var num=101
+
+if(num%2==0){
+    console.log("num is even")
+}
+
+else{
+    console.log("num is odd")
+}
+
+
+
+
+
+
+  
